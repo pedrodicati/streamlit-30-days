55936727ff43a8ab0b1cc4e9f13ccdde1750ea79
Pedro Dicati <phgedi48@gmail.com>
2023-01-16 16:21:31 -0300

    insert days 1 and 2

---
+++ day_1/introduction.md
@@ -0,0 +1,4 @@
+# Introdução sobre o Streamlit
+
+- Para rodar é usado o comando streamlit
+- Explicação de como instalar e rodar o streamlit somente
---
+++ day_2/streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# write escreve na tela
+st.write('Hello, World!')
+
+# para rodar, usar o comando -> streamlit run path_file/file_name
