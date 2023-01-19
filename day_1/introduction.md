bbb628775457a7d04431888a01a13afc38cd523c
Pedro Dicati <phgedi48@gmail.com>
2023-01-19 09:00:17 -0300

    update names of folders

--- day_1/introduction.md
+++
@@ -1,4 +0,0 @@
-# Introdução sobre o Streamlit
-
-- Para rodar é usado o comando streamlit
-- Explicação de como instalar e rodar o streamlit somente
--- day_2/streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-# write escreve na tela
-st.write('Hello, World!')
-
-# para rodar, usar o comando -> streamlit run path_file/file_name
